cc167b3e434d891168bce3577e0080147727c6b5
Gaurav Gutholia <gauravgutholia7@gmail.com>
2020-07-07 13:35:07 -0400

    first commit

---
+++ magnific/magnific.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+  $("#gallery-center").magnificPopup({
+    delegate: "a", // child items selector, by clicking on it popup will open
+    type: "image",
+    gallery: {
+      // options for gallery
+      enabled: true,
+    },
+    // other options
+  });
+});
